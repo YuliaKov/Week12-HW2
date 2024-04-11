53fb3c58564db60b0a83f66ade2391ec6e6aadb1
YuliaKov <yulia.kovalchuk90@gmail.com>
2024-04-12 01:03:21 +0200

    corrected function

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const randomWord = document.getElementById('word');
 const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
 function getRandomWord() {
-    const randomLetter1 = Math.floor(Math.random()*alphabet.length);
-    const randomLetter2 = Math.floor(Math.random()*alphabet.length);
-    const randomLetter3 = Math.floor(Math.random()*alphabet.length);
-    const randomLetter4 = Math.floor(Math.random()*alphabet.length);
-    randomWord.textContent = alphabet[randomLetter1] + alphabet[randomLetter2] + alphabet[randomLetter3] + alphabet[randomLetter4];    
-};
+    for (let i = 0; i < 4; i++) {
+        const randomIndex = Math.floor(Math.random()*alphabet.length);
+        randomWord.textContent += alphabet[randomIndex];
+   }
+     };
 
 getRandomWord();
